36cbbab1826e1f81849bac9e5962b9d76845291f
Luminoid <14302010006@fudan.edu.cn>
2022-09-26 11:57:16 -0400

    update add_problem script

--- add_problem.sh
+++ add_problem.sh
@@ -3,24 +3,28 @@
 python_str="Python"
 sqlite_str="SQLite"
 language=$python_str
+difficulty=""
 
-while getopts i:p:l:h flag
+while getopts i:t:l:hEMH flag
 do
     case "${flag}" in
         i) index=${OPTARG};;
-        p) problem=${OPTARG};;
+        t) title=${OPTARG};;
         l) language=${OPTARG};;
-        h) echo "./add_problem.sh -i <problem index> -p <problem name> -l <language>"; exit 0
+        h) echo "./add_problem.sh -i <problem index> -t <problem title> [-E | -M | -H] [-l <language>]"; exit 0;;
+        E) difficulty="Easy";;
+        M) difficulty="Middle";;
+        H) difficulty="Hard";;
     esac
 done
 
-if [ -z ${index} ] || [ -z "${problem}" ]; then
-    echo "Error: index and problem name cannot be null"
+if [ -z ${index} ] || [ -z "${title}" ]; then
+    echo "Error: problem index and title cannot be null"
     exit 1
 fi
 
-filename=${problem//[[:blank:]]/}
-problem_url=$(echo ${problem// /-} | tr '[:upper:]' '[:lower:]')
+filename=${title//[[:blank:]]/}
+problem_url=$(echo ${title// /-} | tr '[:upper:]' '[:lower:]')
 
 add_problem(){
     if [ $language = $python_str ]; then
@@ -42,9 +46,9 @@ add_problem(){
 if __name__ == '__main__':
     solution = Solution()
 " >> $filepath
-        echo "| ${index} | [${problem}](https://leetcode.com/problems/${problem_url}) | [Python](${filepath}) |  |  |" >> Readme.md
+        echo "| ${index} | [${title}](https://leetcode.com/problems/${problem_url}) | [Python](${filepath}) | ${difficulty} |  |" >> Readme.md
     elif [ $language = $sqlite_str ]; then
-        echo "| ${index} | [${problem}](https://leetcode.com/problems/${problem_url}) | [SQL](${filepath}) |  | Database |" >> Readme.md
+        echo "| ${index} | [${title}](https://leetcode.com/problems/${problem_url}) | [SQL](${filepath}) | ${difficulty} | Database |" >> Readme.md
     fi
     echo "File created: ${filepath}"
 }
